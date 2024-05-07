ffecbfed3cfa5bb42ad7f1125cf2b2e5204450ad
xingduanqing <xingduanqing@58.com>
2024-05-07 18:42:27 +0800

    feat: docs

---
+++ packages/docs/src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <VantButton type="primary">test</VantButton>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.less'
+import VantButton from '../../components/button/Button'
+
+export default {
+  components: { VantButton },
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
