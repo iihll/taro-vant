5d65522fefe83df44340ab35e5561e6223f25023
xingduanqing <xingduanqing@58.com>
2024-05-08 16:16:17 +0800

    feat: overlay

--- packages/docs/src/pages/index/index.vue
+++ packages/docs/src/pages/index/index.vue
@@ -1,37 +1,16 @@
 <template>
   <view class="index">
-    <van-tabs>
-      <van-tab title="标签 1">
-        内容 1
-        <template #title>slot title</template>
-      </van-tab>
-      <van-tab title="标签 2">内容 2</van-tab>
-      <van-tab title="标签 3">内容 3</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
-      <van-tab title="标签 5">内容 5</van-tab>
-    </van-tabs>
-    <van-row>
-      <van-col span="8">span: 8</van-col>
-      <van-col span="8">span: 8</van-col>
-      <van-col span="8">span: 8</van-col>
-    </van-row>
-
-    <van-row>
-      <van-col span="4">span: 4</van-col>
-      <van-col span="10" offset="4">offset: 4, span: 10</van-col>
-    </van-row>
-
-    <van-row>
-      <van-col offset="12" span="12">offset: 12, span: 12</van-col>
-    </van-row>
+    <van-button type="primary" text="显示遮罩层" @click="show = true" />
+    <van-overlay :show="show" @click="show = false" />
   </view>
 </template>
 
 <script lang="ts" setup>
-import VanTabs from '@/components/tabs/Tabs'
-import VanTab from '@/components/tab/Tab'
-import VanRow from '@/components/row/Row'
-import VanCol from '@/components/col/Col'
+import VanButton from '@/components/button/Button'
+import VanOverlay from '@/components/overlay/Overlay'
+import { ref } from 'vue';
+
+const show = ref(false);
 </script>
 
 <style>
