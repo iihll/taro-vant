eb01dad57f890698b74809624021b47d8204e1b5
吕超 <1969956387@qq.com>
2024-05-08 17:17:37 +0800

    delete: 删除荣誉components

--- packages/docs/src/components/loading/index.ts
+++
@@ -1,6 +0,0 @@
-import _Loading from './Loading'
-
-export const Loading = _Loading
-export default Loading
-export { loadingProps } from './types'
-export type { LoadingThemeVars, LoadingType, LoadingProps } from './types'
--- packages/docs/src/components/rate/index.ts
+++
@@ -1,6 +0,0 @@
-import _Rate from './Rate'
-
-export const Rate = _Rate
-export default Rate
-export { rateProps } from './types'
-export type { RateThemeVars, RateProps } from './types'
