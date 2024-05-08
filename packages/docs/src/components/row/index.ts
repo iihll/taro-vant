ed1e9ddbf27869ed8accdcb63c202e8d58940fb6
xingduanqing <xingduanqing@58.com>
2024-05-08 16:10:18 +0800

    feat: row col

---
+++ packages/docs/src/components/row/index.ts
@@ -0,0 +1,12 @@
+import _Row from './Row';
+
+export const Row = _Row
+export default Row;
+export { rowProps } from './Row';
+export type { RowProps, RowAlign, RowJustify } from './Row';
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    VanRow: typeof Row;
+  }
+}
--- packages/docs/src/pages/index/index.vue
+++ packages/docs/src/pages/index/index.vue
@@ -8,13 +8,30 @@
       <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
     </van-tabs>
+    <van-row>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+    </van-row>
+
+    <van-row>
+      <van-col span="4">span: 4</van-col>
+      <van-col span="10" offset="4">offset: 4, span: 10</van-col>
+    </van-row>
+
+    <van-row>
+      <van-col offset="12" span="12">offset: 12, span: 12</van-col>
+    </van-row>
   </view>
 </template>
 
 <script lang="ts" setup>
 import VanTabs from '@/components/tabs/Tabs'
 import VanTab from '@/components/tab/Tab'
+import VanRow from '@/components/row/Row'
+import VanCol from '@/components/col/Col'
 </script>
 
 <style>
