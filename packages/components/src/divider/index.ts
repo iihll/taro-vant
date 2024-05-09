d12a6c692b682cdfcafd799535ff068c92f3335e
吕超 <1969956387@qq.com>
2024-05-09 18:00:47 +0800

    feat: add divider component

---
+++ packages/components/src/divider/index.ts
@@ -0,0 +1,6 @@
+import _Divider from './Divider'
+
+export const Divider = _Divider
+export default Divider
+export { dividerProps } from './types'
+export type { DividerThemeVars, DividerProps, DividerContentPosition } from './types'
